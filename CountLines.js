125dfc2b1b514091250bf0ff08fa9ef5e84226ca
Marcell Purham <mpurham@cybercell.local>
2022-07-07 00:02:26 -0500

    Adding count lines script

---
+++ CountLines.js
@@ -0,0 +1,10 @@
+/*
+    name: Count Lines
+    description: Get the line count of your text
+    author: andipaetzold
+    tags: count,length,size,line
+*/
+
+function main(input) {
+	input.text = `${input.text.split('\n').length} lines`;
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Count Lines
+
+A simple count lines script for the [Polypad](https://polypad.io) Textpad.
+
